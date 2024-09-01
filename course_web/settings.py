7bd2049268f2a8fd3daef0cfbe34cd932979d532
andjey777 <andjey_777@mail.ru>
2024-09-01 17:40:54 +0300

    docker and postgres

--- course_web/settings.py
+++ course_web/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +76,13 @@ WSGI_APPLICATION = "course_web.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": "postgres",
+        "NAME": os.environ.get('POSTGRES_DB'),
+        "USER": os.environ.get('POSTGRES_USER'),
+        "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
+        "PORT": 5432,
+        "OPTIONS": {},
     }
 }
 
