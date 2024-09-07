877e0d22eec46ee86dfa756cf1831e0a4444f5ea
andjey777 <andjey_777@mail.ru>
2024-09-07 12:44:44 +0300

    templates added

---
+++ courses/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+#app_name = "courses"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.Logout_View, name="logout"),
+]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.views import LoginView
+
+from courses.forms import UserLoginForm, UserRegisterForm
 
 # Create your views here.
+
+class IndexView(generic.View):
+    pass
+
+
+class LoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = "courses/login.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
+
+    def get_success_url(self):
+        return reverse_lazy("courses:index")
+
+class RegisterView(generic.CreateView):
+    form_class = UserRegisterForm
+    template_name = 'courses/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('courses:index')
+
+def Logout_View(request):
+    logout(request)
+    return redirect("courses:index")
+
